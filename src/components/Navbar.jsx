31de6a6c8dff8915b2bbbbe9a75274610c9da3da
Specktreon <lezzar37@gmail.com>
2025-01-28 23:12:18 +0100

    Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import { chercherImg, sacImg, appleImg } from "../utils";
+import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex <-full screen-max-width">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" className="h-[18px] w-[14px]" />
 
-        <div>
-          {["Phones", "Macbooks", "Tablets"].map((nav) => (
-            <div key={nav}>{nav}</div>
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
+              {nav}
+            </div>
           ))}
         </div>
 
-        <div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:felx-1">
           <img src={chercherImg} alt="chercher" width={18} height={18} />
           <img src={sacImg} alt="sac" width={18} height={18} />
         </div>
