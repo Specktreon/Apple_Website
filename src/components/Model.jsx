7fb1608a35bea1cea15c5a6ac9761429131bda4b
Specktreon <lezzar37@gmail.com>
2025-02-08 22:47:33 +0100

    both models

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,31 @@
 // import React from 'react'
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useRef, useState } from "react";
+import { yellowImg } from "../utils";
+
+import * as THREE from "three";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useRef({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "FFE789", "#6F6C64"],
+    img: yellowImg,
+  });
+
+  // camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
@@ -13,6 +36,30 @@ const Model = () => {
         <h1 id="heading" className="section-heading">
           Take a closer look.
         </h1>
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full [h-75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
